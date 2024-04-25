470214c961465f2e08228dc2245a7850a0eaa32b
bilalasali <bilalasalioglu9@gmail.com>
2024-04-25 14:24:31 +0300

    1

--- src/P04.java
+++ src/P04.java
@@ -1,6 +1,7 @@
 public class P04 {
     public static void main(String[] args) {
         System.out.println("Ne yaparsan yap aşk ile yap");
+        System.out.println("versiyon olsun diye");
 
     }
 }
